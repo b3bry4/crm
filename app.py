5eda599273275989f007dca50ee0525f8b56818d
root <root@nlpaxatlink.com>
2026-02-06 21:02:34 +0000

    1

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-
-#uvicorn app:app --reload --port 8010 --host 0.0.0.0
-
-
 from fastapi import FastAPI
 
-app=FastAPI()
-
+app = FastAPI()
 
 @app.get("/")
-
 def root():
     return {"Hello": "World"}
