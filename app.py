689d93e5a2b8cb3c92e4014c75cb48a16cb8f247
ChrisHeynJR <troyanovay28@gmail.com>
2026-02-07 00:09:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+users = []
+
+
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+
+@app.post("/users")
+def create_user(user: str):
+    users.append(user)
+    return users
